5b8976ebe80a68c90754bae8196c70f2c56d80d0
zhaomf1024 <zhaomf1024@163.com>
2022-02-28 20:57:53 +0800

    mvc作业

---
+++ mvc/src/main/java/com/lagou/demo/controller/DemoController.java
@@ -0,0 +1,38 @@
+package com.lagou.demo.controller;
+
+import com.lagou.demo.service.IDemoService;
+import com.lagou.edu.mvcframework.annotations.LagouAutowired;
+import com.lagou.edu.mvcframework.annotations.LagouController;
+import com.lagou.edu.mvcframework.annotations.LagouRequestMapping;
+import com.lagou.edu.mvcframework.annotations.Security;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@LagouController
+@LagouRequestMapping("/demo")
+public class DemoController {
+
+
+    @LagouAutowired
+    private IDemoService demoService;
+
+
+    /**
+     * URL: /demo/query?name=lisi
+     * @param request
+     * @param response
+     * @param name
+     * @return
+     */
+    @LagouRequestMapping("/query")
+    public String query(HttpServletRequest request, HttpServletResponse response,String name) {
+        return demoService.get(name);
+    }
+
+    @Security({"lisi","zhangsan"})
+    @LagouRequestMapping("/query2")
+    public String query2(HttpServletRequest request, HttpServletResponse response,String name) {
+        return demoService.get(name);
+    }
+}
---
+++ mvc/src/main/java/com/lagou/edu/mvcframework/annotations/Security.java
@@ -0,0 +1,13 @@
+package com.lagou.edu.mvcframework.annotations;
+
+
+import java.lang.annotation.*;
+
+@Documented
+@Target({ElementType.TYPE,ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Security {
+
+    String[] value();
+
+}
